6cf516975587130a79eb36a483d8f29feb112c16
Apoorva Kashi <apoorvakashi@Apoorvas-MacBook-Air-2.local>
2025-03-07 17:35:31 -0500

    Fix

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.run(host='0.0.0.0', port=5000)
+
 @app.route("/")
 def hello_world():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Run on all available IP addresses (0.0.0.0) and specify the port
+    app.run(debug=True, host='0.0.0.0', port=5000)
